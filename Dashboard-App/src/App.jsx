54054acff2698411e11eb45907dcd04943c75381
Tory Wash <washt@uni.edu>
2026-06-20 20:02:50 -0500

    updated file structure for new development direction

--- Dashboard-App/src/App.jsx
+++ Dashboard-App/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-// build the app
+// app 
 function App() {
 
-  // what displays in the app
   return (
     <>
       <section id="main">
@@ -14,5 +13,5 @@ function App() {
   )
 }
 
-// sends to main to be displayed
+// show the app to the 
 export default App
