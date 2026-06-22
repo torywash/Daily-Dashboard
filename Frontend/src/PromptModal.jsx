efda0a6fa40930ddf8f31c868d9fad2b28ea85e5
Tory Wash <washt@uni.edu>
2026-06-21 22:17:00 -0500

    updated the Prompt UI

--- Frontend/src/PromptModal.jsx
+++ Frontend/src/PromptModal.jsx
@@ -23,16 +23,15 @@ function PromptModal({ onSubmit }) {
                 <h1>What Should We Call You?</h1>
                 <form id="getUserName" onSubmit={handleSubmit}>
                     <label>
-                    Name
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="The Beginning!"
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}
-                    />
-                </label>
-                <button type="submit">Sign The Papers!</button>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="We're So Excited..."
+                            value={name}
+                            onChange={(event) => setName(event.target.value)}
+                        />
+                    </label>
+                    <button type="submit">Sign The Papers!</button>
                 </form>
             </div>
         </div>
